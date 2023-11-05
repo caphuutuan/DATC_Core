86defd3986689cafc2d01216ec40bd0619effdb8
caphuutuan <85669549+caphuutuan@users.noreply.github.com>
2023-11-06 03:22:46 +0700

    create layout client nonFunc

---
+++ DATC_Core/Controllers/ProductController.cs
@@ -0,0 +1,34 @@
+﻿using DATC_Core.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace DATC_Core.Controllers
+{
+	public class ProductController : Controller
+	{
+		private readonly DATCCoreMineDBContext db = new DATCCoreMineDBContext();
+
+		public ProductController(DATCCoreMineDBContext context)
+		{
+			db = context;
+		}
+
+		public IActionResult Index()
+		{
+			return View();
+		}
+		public IActionResult Detail(int id)
+		{
+			var product = db.Products.Include(x => x.CateId).FirstOrDefault(x => x.ProductId == id);
+			if (product ==null)
+			{
+				return RedirectToAction("Index");
+			}
+			else
+			{
+				return View(product);
+
+			}
+		}
+	}
+}
