9ca42af64b005d95ab8d3e24da71d990cc934cdd
caphuutuan <85669549+caphuutuan@users.noreply.github.com>
2023-11-06 19:05:59 +0700

    update layout admin

---
+++ DATC_Core/Controllers/AccountController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DATC_Core.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult LogIn() 
+        {
+            return View();
+        }
+    }
+}
