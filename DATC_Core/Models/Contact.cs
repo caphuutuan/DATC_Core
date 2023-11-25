92eb539f10c7889c8404000087f12df00a093dd6
caphuutuan <85669549+caphuutuan@users.noreply.github.com>
2023-11-25 11:31:13 +0700

    update db

---
+++ DATC_Core/Models/Contact.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DATC_Core.Models
+{
+    public partial class Contact
+    {
+        public int Id { get; set; }
+        public string FullName { get; set; } = null!;
+        public string Email { get; set; } = null!;
+        public string? Phone { get; set; }
+        public string Title { get; set; } = null!;
+        public string? Detail { get; set; }
+        public string? Reply { get; set; }
+        public int Status { get; set; }
+        public DateTime? CreateDate { get; set; }
+        public DateTime? ModifiedDate { get; set; }
+    }
+}
